d96524af9f7714f83c57adeb17cbe627aeb0b360
Sweety Sharma <54081787+sweetie98@users.noreply.github.com>
2021-05-25 17:47:51 +0530

    Update RectangleGarden.java

--- RectangleGarden.java
+++ RectangleGarden.java
@@ -1,22 +1,23 @@
 import java.util.*;
 public class MangoTree {
-    static void checkMangoTree(int rownumber, int columnnumber, int lastcolumn) {
-        //Assuming row and column starts from index 1
-        if (rownumber == 1 || columnnumber == 1 || columnnumber == lastcolumn)
+    static void checkMangoTree(int totalrows, int totalcolumns, int treenumber) {
+        if(treenumber>(totalrows*totalcolumns))
+            System.out.println("Your tree is not in dora's rectangular garden");
+       else if (treenumber<=totalcolumns || treenumber%totalcolumns==0 || treenumber%totalcolumns==1)
             System.out.println("Mango Tree");
-        else
+       else
             System.out.println("Not a Mango Tree");
     }
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter last column number");
-        int lastcolumnnumber = sc.nextInt();
-        System.out.println("Enter tree row number");
-        int treerownumber = sc.nextInt();
-        System.out.println("Enter tree column number");
-        int treecolumnnumber = sc.nextInt();
-        checkMangoTree(treerownumber, treecolumnnumber, lastcolumnnumber);
+        System.out.println("Enter total number of rows");
+        int totalrows = sc.nextInt();
+        System.out.println("Enter total number of columns");
+        int totalcolumns = sc.nextInt();
+        System.out.println("Enter tree number");
+        int treenumber = sc.nextInt();
+        checkMangoTree(totalrows, totalcolumns, treenumber);
 
     }
 }
