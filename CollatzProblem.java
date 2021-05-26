9106a2111ceb554b62845a79ab828a61fb2bed65
Sweety Sharma <54081787+sweetie98@users.noreply.github.com>
2021-05-26 14:24:06 +0530

    Create CollatzProblem.java

---
+++ CollatzProblem.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Collatz {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the sequence starting number");
+      int startingNumber = sc.nextInt();
+      int tempVariable = startingNumber;
+      while(true)
+      {
+          if(tempVariable==1)
+          {
+              System.out.print(tempVariable);
+              break;
+          }
+          System.out.print(tempVariable+"->");
+          tempVariable=(tempVariable%2==0)?tempVariable/2:(3*tempVariable+1);
+      }
+      
+    }
+}
