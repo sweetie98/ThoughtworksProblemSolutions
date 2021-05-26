2677d2be2d82b028859ac0fa32494b0badcdf49f
Sweety Sharma <54081787+sweetie98@users.noreply.github.com>
2021-05-26 16:13:40 +0530

    Update CollatzProblem.java

--- CollatzProblem.java
+++ CollatzProblem.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Collatz {
+public class CollatzProblem {
     public static void main(String args[]) {
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter the sequence starting number");
