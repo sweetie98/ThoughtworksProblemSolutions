b5630c7e192b00f9d96b063cc6f2108d0fa0184e
Sweety Sharma <54081787+sweetie98@users.noreply.github.com>
2021-05-27 17:08:55 +0530

    Update and rename CollatzProblem.java to CollatzSequence.java

--- CollatzSequence.java
+++ CollatzSequence.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class CollatzProblem {
+public class CollatzSequence {
     public static void main(String args[]) {
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter the sequence starting number");
